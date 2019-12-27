d636c04dc381253bde11e4db06e0340341586505
David Fleury <david.fleury@expandium.com>
2019-12-27 16:20:47 +0100

    day 15 - Random discovery of the maze

---
+++ 2019/day_15_oxygen_system_1.cpp
@@ -0,0 +1,296 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <array>
+#include <stack>
+
+using namespace std;
+
+enum class movement { north = 1, south = 2, west = 3, east = 4 };
+const char* movement_str[] = { "", "north", "south", "west", "east"};
+
+enum class status { hit_wall = 0, has_moved = 1, has_moved_and_repair = 2 };
+
+
+stack<movement> movements;
+
+constexpr int SIZE = 50;
+
+std::array<std::array<short, SIZE>, SIZE> grid;
+
+const char* player = "D";
+
+int position_x = SIZE / 2, position_y = SIZE / 2;
+
+movement last_movement = movement::north;
+
+void
+display_grid()
+{
+    cout << "x: " << position_x << ", y:" << position_y << "        stack size: " << movements.size() << endl;
+    cout << string(SIZE, '-') << endl;
+    for (int y = 0; y < SIZE; ++y) {
+        for (int x = 0; x < SIZE; ++x) {
+            if (grid[y][x] == 2) cout << 'O';
+            else if (x == position_x && y == position_y) {
+                cout << player;
+            }
+            else if (x == SIZE / 2 && y == SIZE / 2) {
+                cout << 'x';
+            }
+            else {
+                if (grid[y][x] == 0) cout << '.';
+                else if (grid[y][x] == 1) cout << '#';
+                else if (grid[y][x] == 2) cout << 'O';
+                else cout << ' ';
+            }
+        }
+        cout << endl;
+    }
+}
+
+
+bool is_grid_empty(int y, int x) {
+    return grid[y][x] != 1;
+}
+
+bool is_north_empty() { return is_grid_empty(position_y-1, position_x); }
+bool is_south_empty() { return is_grid_empty(position_y+1, position_x); }
+bool is_east_empty() { return is_grid_empty(position_y, position_x+1); }
+bool is_west_empty() { return is_grid_empty(position_y, position_x-1); }
+
+
+
+
+
+long long relative_base = 0;
+
+enum class parameter_mode { position, immediate, relative };
+
+long long
+get_op_code(long long value)
+{
+    std::string s = std::to_string(value);
+    while(s.size() < 5) s = "0" + s;
+    
+    return std::stoi(s.substr(s.size() - 2));
+}
+
+parameter_mode
+get_mode(long long value, int index)
+{
+    std::string s = std::to_string(value);
+    while(s.size() < 5) s = "0" + s;
+
+    switch(s[2 - index]) {
+        case '1': return parameter_mode::immediate;
+        case '2': return parameter_mode::relative;
+    }
+
+    return parameter_mode::position;
+}
+
+long long
+get_value(vector<long long>& op_codes, int index, parameter_mode mode)
+{
+    switch(mode) {
+    case parameter_mode::position: return op_codes[op_codes[index]];
+    case parameter_mode::relative: return op_codes[op_codes[index] + relative_base];
+    }
+
+    return op_codes[index];
+}
+
+void
+compute(vector<long long>& op_codes)
+{
+    for(size_t i = 0; i < op_codes.size();) {
+
+        auto op_code = get_op_code(op_codes[i]);
+        auto mode_1 = get_mode(op_codes[i], 0);
+        auto mode_2 = get_mode(op_codes[i], 1);
+        auto mode_3 = get_mode(op_codes[i], 2);
+
+        switch(op_code) {
+            case 1: { // +
+                op_codes[mode_3 == parameter_mode::immediate ? i + 3 : op_codes[i + 3] + (mode_3 == parameter_mode::relative ? relative_base : 0)] =
+                        get_value(op_codes, i + 1, mode_1) +
+                        get_value(op_codes, i + 2, mode_2);
+                i += 4;
+                break;
+            }
+            case 2: { // *
+                op_codes[mode_3 == parameter_mode::immediate ? i + 3 : op_codes[i + 3] + (mode_3 == parameter_mode::relative ? relative_base : 0)] =
+                        get_value(op_codes, i + 1, mode_1) *
+                        get_value(op_codes, i + 2, mode_2);
+                i += 4;
+                break;
+            }
+            case 3: { // cin
+                if (movements.empty()) return;
+
+                last_movement = movements.top();
+                movements.pop();
+//                cout << "last_movement: " << movement_str[(int)last_movement] << endl;
+
+                if (mode_1 == parameter_mode::relative) {
+                    op_codes.at(op_codes[i + 1] + relative_base) = (int)last_movement;
+                } else {
+                    op_codes[op_codes[i + 1]] = (int)last_movement;
+                }
+                i += 2;
+                break;
+            }
+            case 4: { // cout
+                int output = get_value(op_codes, i + 1, mode_1);
+//                cout << "status: " << output <<  endl;
+
+                if (output == (int)status::hit_wall) {
+                    switch(last_movement) {
+                    case movement::north:
+                        grid[position_y - 1][position_x] = 1;
+                        break;
+                    case movement::south:
+                        grid[position_y + 1][position_x] = 1;
+                        break;
+                    case movement::east:
+                        grid[position_y][position_x + 1] = 1;
+                        break;
+                    case movement::west:
+                        grid[position_y][position_x - 1] = 1;
+                        break;
+                    }
+                } else {
+                    switch(last_movement) {
+                    case movement::north:
+                        --position_y;
+//                        if (is_south_empty()) movements.push(movement::south);
+//                        if (is_west_empty())  movements.push(movement::west);
+//                        if (is_east_empty())  movements.push(movement::east);
+//                        if (is_north_empty()) movements.push(movement::north);
+                    break;
+                    case movement::south:
+//                        if (is_north_empty()) movements.push(movement::north);
+//                        if (is_east_empty()) movements.push(movement::east);
+//                        if (is_west_empty()) movements.push(movement::west);
+//                        if (is_south_empty()) movements.push(movement::south);
+                        ++position_y;
+                    break;
+                    case movement::east:
+                        ++position_x;
+//                        if (is_west_empty()) movements.push(movement::west);
+//                        if (is_south_empty()) movements.push(movement::south);
+//                        if (is_east_empty()) movements.push(movement::east);
+//                        if (is_north_empty()) movements.push(movement::north);
+                    break;
+                    case movement::west:
+                        --position_x;
+//                        if (is_east_empty()) movements.push(movement::east);
+//                        if (is_south_empty()) movements.push(movement::south);
+//                        if (is_west_empty()) movements.push(movement::west);
+//                        if (is_north_empty()) movements.push(movement::north);
+                        break;
+                    }
+                    grid[position_y][position_x] = (output == (int)status::has_moved_and_repair ? 2 : 0);
+                }
+
+                bool has_push = false;
+                do {
+                switch (rand() % 4 + 1) {
+                case 1:
+                    if (is_north_empty()) {
+                        movements.push(movement::north);
+                        has_push = true;
+                    }
+                    break;
+                case 2:
+                    if (is_south_empty()) {
+                        movements.push(movement::south);
+                        has_push = true;
+                    }
+                    break;
+                case 3:
+                    if (is_east_empty()) {
+                        movements.push(movement::east);
+                        has_push = true;
+                    }
+                    break;
+                case 4:
+                    if (is_west_empty()) {
+                        movements.push(movement::west);
+                        has_push = true;
+                    }
+                    break;
+                }
+            } while(!has_push);
+
+                if (output == (int)status::has_moved_and_repair) {
+                    display_grid();
+                    return;
+                }
+
+                i += 2;
+                break;
+            }
+            case 5: { // jump-if-true
+                if (get_value(op_codes, i + 1, mode_1)) {
+                    i = get_value(op_codes, i + 2, mode_2);
+                } else {
+                    i += 3;
+                }
+                break;
+            }
+            case 6: { // jump-if-false
+                if (!get_value(op_codes, i + 1, mode_1)) {
+                    i = get_value(op_codes, i + 2, mode_2);
+                } else {
+                    i += 3;
+                }
+                break;
+            }
+            case 7: { // less than
+                op_codes[mode_3 == parameter_mode::immediate ? i + 3 : op_codes[i + 3] + (mode_3 == parameter_mode::relative ? relative_base : 0)] =
+                        get_value(op_codes, i + 1, mode_1) < get_value(op_codes, i + 2, mode_2) ? 1 : 0;
+                i += 4;
+                break;
+            }
+            case 8: { // equals
+                op_codes[mode_3 == parameter_mode::immediate ? i + 3 : op_codes[i + 3] + (mode_3 == parameter_mode::relative ? relative_base : 0)] =
+                        get_value(op_codes, i + 1, mode_1) == get_value(op_codes, i + 2, mode_2) ? 1 : 0;
+                i += 4;
+                break;
+            }
+            case 9: { // adjust the relative base
+                relative_base +=  get_value(op_codes, i + 1, mode_1);
+                i += 2;
+                break;
+            }
+            case 99:
+                return;
+        }
+    }
+}
+
+int
+main(int argc, char* argv[])
+{
+    for (auto& r : grid) r.fill(-1);
+    grid[position_y][position_x] = 0;
+
+    movements.push(movement::west);
+    movements.push(movement::south);
+    movements.push(movement::east);
+    movements.push(movement::north);
+
+    ifstream file(argv[1]);
+
+    vector<long long> op_codes;
+    for(long long op_code = 0;file >> op_code; file.ignore(512, ',')) {
+        op_codes.push_back(op_code);
+    }
+    
+    while(op_codes.size() < 100000) op_codes.push_back(0);
+
+    compute(op_codes);
+}
