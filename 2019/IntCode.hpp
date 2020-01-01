a99f787a010a8ee226a12e0d7a71c653efe33c2d
D. Fleury <dfleury2@free.fr>
2020-01-01 22:03:30 +0100

    day 19 part two - almost

---
+++ 2019/IntCode.hpp
@@ -0,0 +1,134 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+// ---------------------------   IntCode   -----------------------------------
+class IntCode {
+public:
+    using OpCodes = std::vector<long long>;
+    OpCodes op_codes;
+
+private:
+    long long relative_base = 0;
+
+    enum class parameter_mode { position, immediate, relative };
+
+    long long get_op_code(long long value) const
+    {
+        std::string s = std::to_string(value);
+        while (s.size() < 5) s = "0" + s;
+
+        return std::stoi(s.substr(s.size() - 2));
+    }
+
+    parameter_mode get_mode(long long value, int index) const
+    {
+        std::string s = std::to_string(value);
+        while (s.size() < 5) s = "0" + s;
+
+        switch (s[2 - index]) {
+        case '1': return parameter_mode::immediate;
+        case '2': return parameter_mode::relative;
+        }
+
+        return parameter_mode::position;
+    }
+
+    long long get_value(std::vector<long long>& op_codes, int index, parameter_mode mode) const
+    {
+        switch (mode) {
+        case parameter_mode::position: return op_codes[op_codes[index]];
+        case parameter_mode::relative: return op_codes[op_codes[index] + relative_base];
+        }
+
+        return op_codes[index];
+    }
+
+public:
+    virtual long long get_input() = 0;
+    virtual void get_output(long long output) = 0;
+
+    void compute()
+    {
+        for (long long i = 0; i < (long long)op_codes.size();) {
+
+            auto op_code = get_op_code(op_codes[i]);
+            auto mode_1 = get_mode(op_codes[i], 0);
+            auto mode_2 = get_mode(op_codes[i], 1);
+            auto mode_3 = get_mode(op_codes[i], 2);
+
+            switch (op_code) {
+            case 1: { // +
+                op_codes[mode_3 == parameter_mode::immediate ? i + 3 : op_codes[i + 3] + (mode_3 == parameter_mode::relative ? relative_base : 0)] =
+                    get_value(op_codes, i + 1, mode_1) +
+                    get_value(op_codes, i + 2, mode_2);
+                i += 4;
+                break;
+            }
+            case 2: { // *
+                op_codes[mode_3 == parameter_mode::immediate ? i + 3 : op_codes[i + 3] + (mode_3 == parameter_mode::relative ? relative_base : 0)] =
+                    get_value(op_codes, i + 1, mode_1) *
+                    get_value(op_codes, i + 2, mode_2);
+                i += 4;
+                break;
+            }
+            case 3: { // cin
+                int input = get_input();
+
+                if (mode_1 == parameter_mode::relative) {
+                    op_codes.at(op_codes[i + 1] + relative_base) = input;
+                }
+                else {
+                    op_codes[op_codes[i + 1]] = input;
+                }
+                i += 2;
+                break;
+            }
+            case 4: { // cout
+                long long output = get_value(op_codes, i + 1, mode_1);
+                get_output(output);
+                i += 2;
+                break;
+            }
+            case 5: { // jump-if-true
+                if (get_value(op_codes, i + 1, mode_1)) {
+                    i = get_value(op_codes, i + 2, mode_2);
+                }
+                else {
+                    i += 3;
+                }
+                break;
+            }
+            case 6: { // jump-if-false
+                if (!get_value(op_codes, i + 1, mode_1)) {
+                    i = get_value(op_codes, i + 2, mode_2);
+                }
+                else {
+                    i += 3;
+                }
+                break;
+            }
+            case 7: { // less than
+                op_codes[mode_3 == parameter_mode::immediate ? i + 3 : op_codes[i + 3] + (mode_3 == parameter_mode::relative ? relative_base : 0)] =
+                    get_value(op_codes, i + 1, mode_1) < get_value(op_codes, i + 2, mode_2) ? 1 : 0;
+                i += 4;
+                break;
+            }
+            case 8: { // equals
+                op_codes[mode_3 == parameter_mode::immediate ? i + 3 : op_codes[i + 3] + (mode_3 == parameter_mode::relative ? relative_base : 0)] =
+                    get_value(op_codes, i + 1, mode_1) == get_value(op_codes, i + 2, mode_2) ? 1 : 0;
+                i += 4;
+                break;
+            }
+            case 9: { // adjust the relative base
+                relative_base += get_value(op_codes, i + 1, mode_1);
+                i += 2;
+                break;
+            }
+            case 99:
+                return;
+            }
+        }
+    }
+};
---
+++ 2019/day_19_tractor_beam_2.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <string>
+#include <iomanip>
+
+#include "IntCode.hpp"
+
+using namespace std;
+
+class BeamDetector : public IntCode
+{
+public:
+    list<int> inputs;
+
+    virtual long long get_input() override {
+        if (inputs.empty()) throw std::runtime_error("Inputs is empty");
+        int input = inputs.front();
+        inputs.pop_front();
+        return input;
+    }
+
+    virtual void get_output(long long value) override {
+        output = value;
+    }
+
+    int output = -1;
+};
+
+
+int
+main(int argc, char* argv[])
+{
+    BeamDetector int_code;
+
+    ifstream file(argv[1]);
+    for (long long op_code = 0; file >> op_code; file.ignore(512, ',')) {
+        int_code.op_codes.push_back(op_code);
+    }
+
+    int position_x = 1500, position_y = 1500;
+
+    list<int> check_positions;
+    const char* msg = "";
+
+    bool check_mode = false;
+    int check_count = 0;
+
+    //for (int y = 210; y < 230; ++y) {
+    //    for (int x = 177; x < 200; ++x) {
+    //        BeamDetector tmp;
+    //        tmp.op_codes = IntCode::OpCodes(5000000, 0);
+    //        memset(&tmp.op_codes[0], 0, tmp.op_codes.size() * sizeof(tmp.op_codes[0]));
+    //        memcpy(&tmp.op_codes[0], &int_code.op_codes[0], int_code.op_codes.size() * sizeof(tmp.op_codes[0]));
+
+    //        tmp.inputs.push_back(x);
+    //        tmp.inputs.push_back(y);
+
+    //        tmp.compute();
+
+    //        cout << (tmp.output ? "#" : ".");
+    //    }
+    //    cout << endl;
+    //}
+
+    for (;;) {
+        BeamDetector tmp;
+        tmp.op_codes = IntCode::OpCodes(5000000, 0);
+        memset(&tmp.op_codes[0], 0, tmp.op_codes.size() * sizeof(tmp.op_codes[0]));
+        memcpy(&tmp.op_codes[0], &int_code.op_codes[0], int_code.op_codes.size() * sizeof(tmp.op_codes[0]));
+
+        if (!check_mode) {
+            // just looking for the
+            msg = "find...";
+            tmp.inputs.push_back(position_x);
+            tmp.inputs.push_back(position_y);
+        }
+        else {
+            // check left/top and left/bottom for the ship
+            tmp.inputs.push_back(check_positions.front());
+            check_positions.pop_front();
+
+            tmp.inputs.push_back(check_positions.front());
+            check_positions.pop_front();
+        }
+
+        tmp.compute();
+
+        cout << msg << " : " << position_x << ", " << position_y << " = " << (tmp.output ? "*" : " ")
+            << "    " << check_positions.size() << "   " << check_count
+            << endl;
+
+        if (!check_mode) {
+            if (tmp.output == 1) {
+                check_mode = true;
+                check_count = 0;
+
+                msg = "check left/top and left/bottom";
+                check_positions.push_back(position_x - 100);
+                check_positions.push_back(position_y);
+
+                check_positions.push_back(position_x - 100);
+                check_positions.push_back(position_y + 100);
+
+                position_x++;
+            }
+            else {
+                position_y++;
+            }
+        }
+        else {  // check mode
+            if (tmp.output == 1) {
+                check_count += 1;
+
+                if (check_count == 2) {
+                    cout << "wahou: " << position_x << ", " << position_y << endl;
+                    cout << (position_x - 101) * 10000 + position_y << endl;
+                    break;
+                }
+            }
+            else {
+                check_mode = false;
+                check_positions.clear();
+                check_count = 0;
+            }
+        }
+    }
+}
