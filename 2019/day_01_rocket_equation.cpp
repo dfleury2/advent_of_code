86865ad39d64a9eede58cc4280a5ceef8d5d61b4
David Fleury <david.fleury@expandium.com>
2020-01-06 12:10:49 +0100

    Tries to improve doctest integration

--- 2019/day_01_rocket_equation.cpp
+++ 2019/day_01_rocket_equation.cpp
@@ -1,4 +1,5 @@
 #include <istream>
+#include <fstream>
 
 #define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest/doctest.h>
@@ -8,48 +9,54 @@ using namespace std;
 const char* filename = nullptr;
 
 // --- part one ---
-int compute_fuel(istream&& is) {
-    int total_fuel = 0;
-    for(int mass = 0;is >> mass;)
-        total_fuel += mass / 3 - 2;
-    return total_fuel;
-}
+struct FuelComputer {
+    virtual ~FuelComputer() = default;
+
+    int compute_fuel(istream&& is) {
+        int total_fuel = 0;
+        for(int mass = 0;is >> mass;)
+            total_fuel += fuel(mass);
+        return total_fuel;
+    }
+
+    virtual int fuel(int mass) {
+        return mass / 3 - 2;
+    }
+};
 
 // --- part two ---
-int fuel(int v) {
-    int f = v /3 - 2;
-    if (f <= 0) return 0;
-    return f + fuel(f);
-}
-
-int compute_fuel2(istream&& is) {
-    int total_fuel = 0;
-    for(int mass = 0;is >> mass;)
-        total_fuel += fuel(mass);
-    return total_fuel;
-}
+struct FuelComputer2 : FuelComputer {
+    int fuel(int v) override {
+        int f = v /3 - 2;
+        return (f <= 0 ? 0 : f + fuel(f));
+    }
+};
 
 // --------------------------------------------------------------------------
 TEST_CASE("Part one examples") {
-    CHECK(compute_fuel(istringstream("12")) == 2);
-    CHECK(compute_fuel(istringstream("14")) == 2);
-    CHECK(compute_fuel(istringstream("1969")) == 654);
-    CHECK(compute_fuel(istringstream("100756")) == 33583);
+    FuelComputer fc;
+    CHECK(fc.compute_fuel(istringstream("12")) == 2);
+    CHECK(fc.compute_fuel(istringstream("14")) == 2);
+    CHECK(fc.compute_fuel(istringstream("1969")) == 654);
+    CHECK(fc.compute_fuel(istringstream("100756")) == 33583);
 }
 
 TEST_CASE("Part one") {
-    CHECK(compute_fuel(ifstream(filename)) == 3160932);
+    FuelComputer fc;
+    CHECK(fc.compute_fuel(ifstream(filename)) == 3160932);
 }
 
 // --------------------------------------------------------------------------
 TEST_CASE("Part two examples") {
-    CHECK(compute_fuel2(istringstream("12")) == 2);
-    CHECK(compute_fuel2(istringstream("1969")) == 966);
-    CHECK(compute_fuel2(istringstream("100756")) == 50346);
+    FuelComputer2 fc;
+    CHECK(fc.compute_fuel(istringstream("12")) == 2);
+    CHECK(fc.compute_fuel(istringstream("1969")) == 966);
+    CHECK(fc.compute_fuel(istringstream("100756")) == 50346);
 }
 
 TEST_CASE("Part two") {
-    CHECK(compute_fuel2(ifstream(filename)) == 4738549);
+    FuelComputer2 fc;
+    CHECK(fc.compute_fuel(ifstream(filename)) == 4738549);
 }
 
 // --------------------------------------------------------------------------
@@ -57,20 +64,22 @@ int main(int argc, char* argv[])
 {
     doctest::Context context(argc, argv);
 
-    filename = argv[1];
+    filename = argv[1]; // only to deal with final input filename
 
     int result = context.run();
     if (context.shouldExit())
         return result;
 
     { // part one
-        auto total_fuel = compute_fuel(ifstream(filename));
+        FuelComputer fc;
+        auto total_fuel = fc.compute_fuel(ifstream(filename));
         cout << "Result: " << total_fuel
             << "\n ---> part one: " << (total_fuel == 3160932 ? "Success" : "FAILED") << endl;
     }
 
     { // part two
-        auto total_fuel = compute_fuel2(ifstream(filename));
+        FuelComputer2 fc;
+        auto total_fuel = fc.compute_fuel(ifstream(filename));
         cout << "Result: " << total_fuel
             << "\n ---> part two: " << (total_fuel == 4738549 ? "Success" : "FAILED") << endl;
     }
